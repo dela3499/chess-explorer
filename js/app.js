4d92ae7dc23ee135f3669738706bcd3d2b243d4f
Carlos <dela3499@gmail.com>
2014-07-23 18:36:37 -0500

    Started putting page together. Rethinking interface.

--- js/app.js
+++ js/app.js
@@ -1,79 +1,92 @@
-var board,
-  game = new Chess(),
-  statusEl = $('#status'),
-  fenEl = $('#fen'),
-  pgnEl = $('#pgn');
+/** @jsx React.DOM */
 
-// do not pick up pieces if the game is over
-// only pick up pieces for the side to move
-var onDragStart = function(source, piece, position, orientation) {
-  if (game.game_over() === true ||
-      (game.turn() === 'w' && piece.search(/^b/) !== -1) ||
-      (game.turn() === 'b' && piece.search(/^w/) !== -1)) {
-    return false;
-  }
-};
+var FluxMixin = Fluxxor.FluxMixin(React),
+    FluxChildMixin = Fluxxor.FluxChildMixin(React)
+    StoreWatchMixin = Fluxxor.StoreWatchMixin;
 
-var onDrop = function(source, target) {
-  // see if the move is legal
-  var move = game.move({
-    from: source,
-    to: target,
-    promotion: 'q' // NOTE: always promote to a queen for example simplicity
-  });
-
-  // illegal move
-  if (move === null) return 'snapback';
-
-  updateStatus();
-};
-
-// update the board position after the piece snap 
-// for castling, en passant, pawn promotion
-var onSnapEnd = function() {
-  board.position(game.fen());
-};
-
-var updateStatus = function() {
-  var status = '';
-
-  var moveColor = 'White';
-  if (game.turn() === 'b') {
-    moveColor = 'Black';
-  }
-
-  // checkmate?
-  if (game.in_checkmate() === true) {
-    status = 'Game over, ' + moveColor + ' is in checkmate.';
-  }
-
-  // draw?
-  else if (game.in_draw() === true) {
-    status = 'Game over, drawn position';
-  }
-
-  // game still on
-  else {
-    status = moveColor + ' to move';
-
-    // check?
-    if (game.in_check() === true) {
-      status += ', ' + moveColor + ' is in check';
+var AudioStore = Fluxxor.createStore({
+    // This store manages all audio playback
+    initialize: function () {
+        this.bindActions(
+            "TOGGLE_PLAYBACK", this.togglePlayback
+        );        
+        var store = this; // used to access this scope inside of
     }
-  }
-
-  statusEl.html(status);
-  fenEl.html(game.fen());
-  pgnEl.html(game.pgn());
+});
+var App = React.createClass ({
+    render: function () {
+        return (
+            <div id="app">
+                <OptionsPanel/>
+                <PlayPanel/>
+                <MovePanel/>
+            </div>
+        );
+    }
+});
+var OptionsPanel = React.createClass ({
+    render: function () {
+        return (
+            <div id="options-panel">
+                <h1>CHESS EXPLORER</h1>
+                <h2>about</h2>
+                <h2>help</h2>
+            </div>
+        );
+    }
+});
+var PlayPanel = React.createClass ({
+    componentDidMount: function () {
+        var cfg = {
+          draggable: true,
+          position: 'start',
+          showNotation: false
+//          onDragStart: onDragStart,
+//          onDrop: onDrop,
+//          onSnapEnd: onSnapEnd
+        };
+        board = new ChessBoard('board', cfg);
+    },    
+    render: function () {
+        return (
+            <div id="play-panel">
+                <div id="board"></div>
+            </div>
+        );
+    }
+});
+var MovePanel = React.createClass ({
+    render: function () {
+        return (
+            <div id="move-panel">
+            </div>
+        );
+    }
+});
+var X = React.createClass ({
+    render: function () {
+        return (
+            <div/>
+        );
+    }
+});
+var stores = {
+    AudioStore: new AudioStore()
 };
-
-var cfg = {
-  draggable: true,
-  position: 'start',
-  onDragStart: onDragStart,
-  onDrop: onDrop,
-  onSnapEnd: onSnapEnd
+var actions = {
+    audio: {
+        togglePlayback: function () {
+            this.dispatch("TOGGLE_PLAYBACK", {});
+        }
+    }
+    
 };
-board = new ChessBoard('board', cfg);
+var flux = new Fluxxor.Flux(stores,actions);
+React.renderComponent(
+    <App flux={flux}/>,
+    document.getElementById('content')
+);
 
-updateStatus();
+
+
+
